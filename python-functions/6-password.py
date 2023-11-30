b898b826eb25ad2f33d8d88172c985bb282d9176
rachidelkd <rachidmitos.37@gmail.com>
2023-12-01 00:00:17 +0100

    25

---
+++ python-functions/6-password.py
@@ -0,0 +1,4 @@
+def validate_password(password):
+    return len(password) >= 8 and any(c.isupper() for c in password) \
+           and any(c.islower() for c in password) and any(c.isdigit() for c in password) \
+           and ' ' not in password
